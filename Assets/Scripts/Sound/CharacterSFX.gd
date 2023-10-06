372bd54267ffefc7271933ec0958ac5b9156fc4e
Dev <dev@lotech.info>
2023-10-05 21:04:31 -0700

    Add sound effects and sound assets.

---
+++ Assets/Scripts/Sound/CharacterSFX.gd
@@ -0,0 +1,17 @@
+extends AudioStreamPlayer2D
+
+
+@export var attack: AudioStreamMP3 = null
+@export var hurt: AudioStreamMP3 = null
+@export var death: AudioStreamMP3 = null
+@export var footsteps: AudioStreamMP3 = null
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	attack.loop = false
+	hurt.loop = false
+	death.loop = false
+	footsteps.loop = false
+
+
